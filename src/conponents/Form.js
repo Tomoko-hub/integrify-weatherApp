c10aab8b207edf39d5ccc814ae6fe06634100ee0
Tomoko Uehara <tomoko.uehara@salesframe.com>
2022-02-14 14:40:43 +0200

    implement api

--- src/conponents/Form.js
+++ src/conponents/Form.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import { useState } from 'react';
+import axios from 'axios';
 
 function Form() {
 
     const [ city, setCity ] = useState("");
+    const getWeather = (event)=> {
+        event.preventDefault();
+        axios.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/133328?apikey=ZGMFxx4xhvbrwQgXbRD5qE4Qq1p44zV8")
+        .then(res=> console.log(res));
+    }
 
   return (
     <div className="container mb-3">
@@ -16,9 +22,10 @@ function Form() {
                        id="city"
                        placeholder="Input City"
                        onChange={event => setCity(event.target.value)} />
-                       {city}
                 <button type="submit" 
-                        className="btn btn-primary col">
+                        className="btn btn-primary col"
+                        onClick={getWeather}
+                        >
                         Get Weather
                 </button>
             </form>
