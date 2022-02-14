c2773b97943b2fafc9a754035c1f35f5e826f337
Tomoko Uehara <tomoko.uehara@salesframe.com>
2022-02-14 11:35:30 +0200

    create home-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Homecomponent from './conponents/Homecomponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Tämä on App Component</h1>
+      <Homecomponent />
     </div>
   );
 }
---
+++ src/conponents/Homecomponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homecomponent() {
+  return (
+    <div>
+        <h1>World Weather App</h1>
+    </div>
+  )
+}
+
+export default Homecomponent
